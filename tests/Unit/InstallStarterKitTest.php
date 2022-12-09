f7b79c0c3344d0d0f743743d4c12c9543a195cf9
Eric Woods <elw234@cornell.edu>
2022-12-09 13:54:03 -0500

    Fix failing test

--- tests/Unit/InstallStarterKitTest.php
+++ tests/Unit/InstallStarterKitTest.php
@@ -2,13 +2,13 @@
 
 namespace CUCustomDev\LaravelStarterKit\Tests\Unit;
 
-use CUCustomDev\LaravelStarterKit\Console\InstallStarterKit;
+use CUCustomDev\LaravelStarterKit\StarterKitServiceProvider;
 use PHPUnit\Framework\TestCase;
 
 class InstallStarterKitTest extends TestCase
 {
     public function testStarterKitIncludesReadme()
     {
-        $this->assertContains('README.md', InstallStarterKit::INSTALL_FILES);
+        $this->assertContains('README.md', StarterKitServiceProvider::INSTALL_FILES);
     }
 }
