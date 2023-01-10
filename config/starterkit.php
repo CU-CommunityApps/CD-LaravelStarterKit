a8b1a21b84a73d7f1f722b2815c44213ef3de76f
Rich Marisa <rjm2@cornell.edu>
2023-01-10 13:41:13 -0500

    update config docs

--- config/starterkit.php
+++ config/starterkit.php
@@ -3,17 +3,21 @@
 return [
 
     /* 
-     * The users' netid will be retrieved from an environment variable
+     * The users' netid may be retrieved from an environment variable
      * populated by the Apache shibboleth module.  
-     * The default env variable is REMOTE USER, but this may be e.g, 
-     * REDIRECT_REMOTE_USER on some servers.
      * 
+     * The default env variable is REMOTE USER, but this may be e.g, 
+     * REDIRECT_REMOTE_USER on some servers, depending on how PHP is installed.
+     * 
+     * This configuration allows configuration of the remote_user_variable so it
+     * is not hardcoded in the application. 
+     * 
+     * In project code, use env(config('starterkit.remote_user_variable')) to retrieve the netid
+     *
      * For local development without shibboleth, you can add
      * REMOTE_USER=<netid>
      * to your project .env file to set the user netid. 
-     * 
-     * In project code, use env(config('starterkit.remote_user_variable')) to retrieve the netid
-     * 
+     *
      */
     'remote_user_variable' => env("REMOTE_USER_VARIABLE", "REMOTE_USER"),
 
